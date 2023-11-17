c8d0837d861fcc9e58d28b6b635299be29a19f5c
Yugbrat <72247331+Yugbrat@users.noreply.github.com>
2023-11-18 00:19:52 +1030

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+# Assuming your build script is named 'build.sh' and 'Readme.txt' is the instructions file
+git add build.sh Readme.txt
+git commit -m "Add build automation script and instructions"
+git push origin master
